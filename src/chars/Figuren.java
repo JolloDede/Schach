f0dda7c255d08ddc227a8685421d5dbef7315dfc
JolloDede <jollodede@gmail.com>
2019-05-28 10:35:42 +0200

    progress - v190528-10.35

--- src/chars/Figuren.java
+++ src/chars/Figuren.java
@@ -11,6 +11,42 @@ public abstract class Figuren {
     public Figuren(Feld feld, Spielfeld spielfeld){
         this.feld = feld;
         this.spielfeld = spielfeld;
+
+        if(feld.getY() > 3){
+            this.color = Color.White;
+        }else{
+            this.color = Color.Black;
+        }
     }
+
+    public ArrayList<Feld> getFelderZumBewegen(){
+        return felderZumBewegen;
+    }
+
+    public void printFelderZumBewegen(){
+        System.out.println("Diese Figur kann sich auf diese Felder Bewegen");
+
+        for (Feld feld : felderZumBewegen){
+            if (feld.getFigur() != null){
+                System.out.println(feld.getX() +"/"+ feld.getY() +" hat eine Figur");
+            }else{
+                System.out.println(feld.getX() +"/"+ feld.getY() +"ist frei");
+            }
+        }
+    }
+
+    public Feld getFeld() {
+        return feld;
+    }
+
+    public void setFeld(Feld feld) {
+        this.feld = feld;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public abstract void setFelderBewegen();
 
 }
